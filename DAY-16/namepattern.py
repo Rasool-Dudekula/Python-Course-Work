0656b210807e4951048d214a48ca8c95bef00afd
Rasool Dudekula <rasooldudekula10@gmail.com>
2026-09-23 15:38:12 +0530

    Namepatterns

---
+++ DAY-16/namepattern.py
@@ -0,0 +1,180 @@
+'''
+n = int(input("Enter the number: "))
+for i in range(n):
+    for j in range(n):
+        if i == 0 or j == 0 or i == n-1 or j == n-1:
+            print('*',end = ' ')
+        else:
+            print(' ',end = ' ')
+    print()
+
+n = int(input("Enter the number: "))
+m = n//2
+for i in range(n):
+    for j in range(n):
+        if i == 0 or j == 0 or i == n-1 or j == n-1 or i == m:
+            print('*',end = ' ')
+        else:
+            print(' ',end = ' ')
+    print()
+
+n = int(input("Enter the number: "))
+m = n//2
+for i in range(n):
+    for j in range(n):
+        if i == 0 or j == 0 or i == n-1 or i == m:
+            print('*',end = ' ')
+        else:
+            print(' ',end = ' ')
+    print()
+
+n = int(input("Enter the number: "))
+m = n//2
+for i in range(n):
+    for j in range(n):
+        if i == 0 or j == 0 or  i == m:
+            print('*',end = ' ')
+        else:
+            print(' ',end = ' ')
+    print()
+
+n = int(input("Enter the number: "))
+m = n//2
+for i in range(n):
+    for j in range(n):
+        if i == 0 or j == 0 or i == n-1:
+            print('*',end = ' ')
+        else:
+            print(' ',end = ' ')
+    print()
+
+n = int(input("Enter the number: "))
+m = n//2
+for i in range(n):
+    for j in range(n):
+        if i == 0 or j == 0 or i == n-1 or (j == n-1 and i >= m) or (i == m and j >= m):
+            print('*',end = ' ')
+        else:
+            print(' ',end = ' ')
+    print()
+
+n = int(input("Enter the number: "))
+m = n//2
+for i in range(n):
+    for j in range(n):
+        if i == 0 or i == n-1 or j == m:
+            print('*',end = ' ')
+        else:
+            print(' ',end = ' ')
+    print()
+
+n = int(input("Enter the number: "))
+m = n//2
+for i in range(n):
+    for j in range(n):
+        if i == 0 or (i == n-1 and j <=m) or j == m:
+            print('*',end = ' ')
+        else:
+            print(' ',end = ' ')
+    print()
+
+n = int(input("Enter the number: "))
+m = n//2
+for i in range(n):
+    for j in range(n):
+        if i == 0 or (i == n-1 and j <=m) or j == m:
+            print('*',end = ' ')
+        else:
+            print(' ',end = ' ')
+    print()
+
+n = int(input("Enter the number: "))
+m = n//2
+for i in range(n):
+    for j in range(n):
+        if j == 0 or j == n-1 or i == m:
+            print('*',end = ' ')
+        else:
+            print(' ',end = ' ')
+    print()
+
+n = int(input("Enter the number: "))
+m = n//2
+for i in range(n):
+    for j in range(n):
+        if i == 0 or i == n-1 or i+j == n-1:
+            print('*',end = ' ')
+        else:
+            print(' ',end = ' ')
+    print()
+
+n = int(input("Enter the number: "))
+m = n//2
+for i in range(n):
+    for j in range(n):
+        if i == j or i+j == n-1:
+            print('*',end = ' ')
+        else:
+            print(' ',end = ' ')
+    print()
+
+n = int(input("Enter the number: "))
+m = n//2
+for i in range(n):
+    for j in range(n):
+        if ((i <= m and (j == i or j == n-i-1)) or(i > m and j == m)):
+            print('*',end = ' ')
+        else:
+            print(' ',end = ' ')
+    print()
+
+n = int(input("Enter the number: "))
+m = n//2
+for i in range(n):
+    for j in range(n):
+        if i == 0 or i == n-1 or i+j == n-1:
+            print('*',end = ' ')
+        else:
+            print(' ',end = ' ')
+    print()
+
+n = int(input("Enter the number: "))
+m = n//2
+for i in range(n):
+    for j in range(n):
+        if j == 0 or (i == m and j <= m) or (i == j and i >= m) or (i+j == n-1 and i <= m):
+            print('*',end = ' ')
+        else:
+            print(' ',end = ' ')
+    print()
+
+n = int(input("Enter the number: "))
+m = n//2
+for i in range(n):
+    for j in range(n):
+        if j == 0 or j == n-1 or (i == j and i <= m) or (i+j == n-1 and i <= m):
+            print('*',end = ' ')
+        else:
+            print(' ',end = ' ')
+    print()
+
+n = int(input("Enter the number: "))
+m = n//2
+for i in range(n):
+    for j in range(n):
+        if j == 0 or j == n-1 or (i == j and i <= m) or (i+j == n-1 and i <= m):
+            print('*',end = ' ')
+        else:
+            print(' ',end = ' ')
+    print()
+
+n = int(input("Enter the number: "))
+m = n//2
+for i in range(n):
+    for j in range(n):
+        if j == 0 or i == 0 or i == n-1 or j == n-1 or i == m or j == m:
+            print('*',end = ' ')
+        else:
+            print(' ',end = ' ')
+    print()
+'''
